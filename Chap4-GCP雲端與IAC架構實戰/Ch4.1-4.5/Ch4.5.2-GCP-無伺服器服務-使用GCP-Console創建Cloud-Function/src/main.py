d0fec27d031595f19fbd0452c21545dceb582bdd
tonycclin <156723456+tonycclin@users.noreply.github.com>
2024-01-21 14:34:01 -0500

    Delete Chap4-GCP雲端與IAC架構實戰 directory

--- Chap4-GCP雲端與IAC架構實戰/Ch4.1-4.5/Ch4.5.2-GCP-無伺服器服務-使用GCP-Console創建Cloud-Function/src/main.py
+++
@@ -1,9 +0,0 @@
-from flask import make_response, request
-
-
-def handler(request):
-    headers = {
-        'Content-Type': 'text/plain'
-    }
-    response = make_response('Hello, World!', 200, headers)
-    return response
--- Chap4-GCP雲端與IAC架構實戰/Ch4.6-4.10/Ch4.8.3-GCP-分析服務-使用Terraform創建消息隊列PubSub/test-message.sh
+++
@@ -1,5 +0,0 @@
-gcloud pubsub topics publish \
-"projects/terraform101-384507/topics/pubsub-topic-tf-ch-4-8-3" \
-    --message "Hello, World"
-
-gcloud pubsub subscriptions pull projects/terraform101-384507/subscriptions/pubsub-sub-tf-ch-4-8-3 --limit=10
